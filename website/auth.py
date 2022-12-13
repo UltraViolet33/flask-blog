49a3e01cf7e647b928d23811c60bb77c97dc5a3e
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2022-12-13 18:14:51 +0100

    format code

--- website/auth.py
+++ website/auth.py
@@ -59,10 +59,8 @@ def sign_up():
     return render_template("signup.html", user=current_user)
 
 
-
 @auth.route("/logout")
 @login_required
 def logout():
     logout_user()
-
     return redirect("/login")
--- website/views.py
+++ website/views.py
@@ -36,18 +36,15 @@ def create_post():
 @login_required
 def delete_post(id):
     post = Post.query.filter_by(id=id).first()
-    
-    
+
     if not post:
         flash("Post does not exist.", category="error")
     elif current_user.id != post.id:
-        print(post)
         flash("You don't have permission to delete this post.", category="error")
     else:
         db.session.delete(post)
         db.session.commit()
         flash("Post deleted", category="success")
-    
     return redirect(url_for("views.home"))
 
 
@@ -55,45 +52,40 @@ def delete_post(id):
 @login_required
 def posts(username):
     user = User.query.filter_by(username=username).first()
-    
+
     if not user:
         flash("No user with that username exists.", category="error")
         return redirect(url_for("views.home"))
-    
+
     posts = user.posts
-    
-    return render_template("posts.html", user=current_user, posts=posts, username=username) 
-    
-    
+    return render_template("posts.html", user=current_user, posts=posts, username=username)
+
+
 @views.route("/create-comment/<post_id>", methods=["POST"])
 @login_required
 def create_comment(post_id):
-    
     text = request.form.get("text")
-    
+
     if not text:
         flash("Comment cannot be empty.", category="error")
-    
     else:
         post = Post.query.filter_by(id=post_id)
-    
+
         if post:
-            comment = Comment(text=text, author=current_user.id, post_id=post_id)
+            comment = Comment(
+                text=text, author=current_user.id, post_id=post_id)
             db.session.add(comment)
             db.session.commit()
             flash("Comment created.", category="success")
-            
         else:
             flash("Post does not exist.", category="error")
-            
-    return redirect(url_for("views.home"))
 
+    return redirect(url_for("views.home"))
 
 
 @views.route("/delete-comment/<comment_id>")
 @login_required
 def delete_comment(comment_id):
-    
     comment = Comment.query.filter_by(id=comment_id).first()
     if not comment:
         flash("Comment does not exist.", category="error")
@@ -104,14 +96,14 @@ def delete_comment(comment_id):
         db.session.delete(comment)
         db.session.commit()
     return redirect(url_for("views.home"))
-        
-        
+
 
 @views.route("/like-post/<post_id>", methods=["GET"])
 @login_required
 def like(post_id):
     post = Post.query.filter_by(id=post_id)
-    like = Like.query.filter_by(author=current_user.id, post_id=post_id).first()
+    like = Like.query.filter_by(
+        author=current_user.id, post_id=post_id).first()
     if not post:
         flash("Post does not exist.", category="error")
     elif like:
@@ -121,5 +113,5 @@ def like(post_id):
         like = Like(author=current_user.id, post_id=post_id)
         db.session.add(like)
         db.session.commit()
-        
+
     return redirect(url_for("views.home"))
